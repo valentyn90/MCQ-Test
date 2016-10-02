90749c48172124571298ecc5860f0190e4d9bb6d
itsumrat <itsumrat@gmail.com>
2016-10-02 21:47:38 +0600

    added 2nd page

--- assets/src/js/app.js
+++ assets/src/js/app.js
@@ -1,12 +1,15 @@
 function submitAnswers(e) {
 	e.preventDefault();
-	var total = 3;
+	var total = 6;
 	var score = 0;
 
 	// Get user input
 	var q1 = document.forms["quizForm"]["q1"].value;
 	var q2 = document.forms["quizForm"]["q2"].value;
 	var q3 = document.forms["quizForm"]["q3"].value;
+	var q4 = document.forms["quizForm"]["q4"].value;
+	var q5 = document.forms["quizForm"]["q5"].value;
+	var q6 = document.forms["quizForm"]["q6"].value;
 
 	// Validation
 	for (var i = 1; i <= total; i++) {
@@ -19,7 +22,7 @@ function submitAnswers(e) {
 	}
 
 	// Set correct answer
-	var answers = ["a","b","c"];
+	var answers = ["a","b","c","a","b","c"];
 
 	// Check Answer 
 	for (var i = 1; i <= total; i++) {
